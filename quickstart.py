79582e919768967797744c8aa22db8b880800c99
griperaivos <gripeuchiha@gmail.com>
2024-09-28 19:33:16 -0300

    'quickstart'

--- quickstart.py
+++ quickstart.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, make_response, redirect, request, render_template, url_for
+from flask import Flask, abort, make_response, redirect, request, render_template, session, url_for
 from markupsafe import escape
 from werkzeug.utils import secure_filename
 
@@ -116,3 +116,49 @@ def login():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
+
+"""
+In addition to the request object there is also a second object called 'session' 
+wich allows you to store information specific to a user from
+one request to the next. 
+This is implemented on top of cookies for you and signs the cookies
+cryptographically.
+What this means is that the user could look at the contents of your cookie but not
+modify it, unless they know the secret key used for signing.
+"""
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session['username']}'
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+                <form method="post">
+                    <p><input type=text name=username>
+                    <p><input type=submit value=Login>
+                </form>
+        '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+"""
+Your operating system has ways to generate pretty random data based
+on a cryptographic random generator. Use the following command
+to quickly generate a value for Flask.secret_key (or SECRET_KEY)
+
+python -c "import secrets; print(secrets.token_hex())"
+
+"""
