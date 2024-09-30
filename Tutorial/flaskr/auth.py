8d92a9f1506bd514bbd09ddb95852ebd328ecb31
griperaivos <gripeuchiha@gmail.com>
2024-09-30 17:54:39 -0300

    'Blueprints_and_Views'

--- Tutorial/flaskr/auth.py
+++ Tutorial/flaskr/auth.py
@@ -58,3 +58,71 @@ def register():
         flash(error)
     
     return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        """
+        fetchone() returns one row from the query.
+        If the query returned no results, it returns None.
+        fetchall() will be used, which returns a list of all results
+        """
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+
+            """
+            check_password_hash() hashes the submitted password in the smae way as the stored hash and securely compares them
+            If they match, the password is valid
+            """
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.' 
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+
+    return render_template('auth/login.html')
+
+"""
+bp.before_app_request() registers a function that runs before the view function, no matter what URL is requested.
+load_logged_in_user checks if a user id is stored in the session and gets that user's data from the database, storing it on g.user, which lasts for the lenght of the request.
+If there is no user id, or if the id doesn't exist, g.user will be None
+"""
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+# checks if the user is logged in
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
